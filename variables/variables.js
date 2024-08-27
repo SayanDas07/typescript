03b33cffc65b89e49b2575479d3950e2aeb0e935
Sayan Das <sayandasraja56@gmail.com>
2024-08-28 01:37:20 +0530

    first commit

---
+++ variables/variables.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 'hello';
+console.log(a);
+//number
+var b = 10;
+console.log(b);
+//boolean
+var c = true;
+console.log(c);
+//best practice
+var d = 8;
+console.log(d);
---
+++ variables/variables.ts
@@ -0,0 +1,16 @@
+let a: string = 'hello'
+
+console.log(a)
+//number
+let b: number = 10
+console.log(b)
+
+//boolean
+let c: boolean = true
+console.log(c)
+
+//best practice
+let d = 8
+console.log(d)
+export {}
+
