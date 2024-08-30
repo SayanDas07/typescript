c0d87fff2a3972f589456bc335b93c2637d9d942
Sayan Das <sayandasraja56@gmail.com>
2024-08-30 12:50:58 +0530

    classes

--- TsProjectSetUp/dist/index.js
+++ TsProjectSetUp/dist/index.js
@@ -1,2 +1,50 @@
 "use strict";
-console.log("hii");
+// class User {
+//     //by default all the properties are public
+//     email: string
+//     public name: string
+//     private readonly city: string = "kolkata"
+//     constructor(email: string, name: string) {
+//         this.email = email,
+//         this.name = name
+//     }
+// }
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        //by default all the properties are public
+        this.city = "kolkata";
+    }
+    //getter
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    //getter for courseCount
+    get courseCount() {
+        return this._courseCount;
+    }
+    //setter for courseCount
+    //seter always take one argument and doest not return anything
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+}
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
+    }
+}
+const raja = new User("sd.com", "raja");
+// console.log(raja.city)
--- TsProjectSetUp/src/index.ts
+++ TsProjectSetUp/src/index.ts
@@ -1,1 +1,61 @@
-console.log("hii")
+// class User {
+//     //by default all the properties are public
+//     email: string
+//     public name: string
+//     private readonly city: string = "kolkata"
+//     constructor(email: string, name: string) {
+//         this.email = email,
+//         this.name = name
+//     }
+// }
+
+class User {
+
+    protected _courseCount = 1
+    //by default all the properties are public
+
+    private readonly city: string = "kolkata"
+    constructor(public email: string,
+        private name: string) {
+
+
+    }
+
+    //getter
+    get getAppleEmail(): string {
+        return `apple${this.email}`
+    }
+
+
+    //getter for courseCount
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    //setter for courseCount
+    //seter always take one argument and doest not return anything
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum
+    }
+
+    private deleteToken() {
+        console.log("Token deleted");
+
+    }
+}
+
+
+class SubUser extends User {
+    isFamily: boolean = true
+    changeCourseCount(){
+        this._courseCount = 4
+    }
+}
+
+
+const raja = new User("sd.com", "raja")
+
+// console.log(raja.city)
